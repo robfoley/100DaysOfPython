0afc177fb83c39fc6e063586f4f7ddb002bec288
Rob <rob.foley@icloud.com>
2024-06-09 15:01:06 -0500

    Day 01 of 100 Days Of Python

---
+++ 100DaysOfCode.py
@@ -0,0 +1,30 @@
+#Day 001
+
+print("Hello World")
+print("and how are you?")
+print("😂")
+
+print(""" 
+Here's an exect
+	Bit
+		Of
+			Text
+			""")
+			
+print("""
+Rob
+June 9th 2024
+I am signin up for Replit's 100 days of Pytnon
+challenge!
+I will make sure to spend some time every day coding
+along, for a minumum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do
+this from my phone wherever I happen to be.  No excuses
+for not coding from the middle of a field!
+I am feeling 🙏
+You can follow my progress at replit.com/@iAmMarkProbably""")
+
+#End Day 001
+
+
+#Day 002
